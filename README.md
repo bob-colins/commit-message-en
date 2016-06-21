ec97673334dd6935c6368e4ff9425ab3cf572d77
kosuke sakai <kosuke.s.sakai@gmail.com>
2016-06-21 21:20:23 +0700

    Convert 1 article to docset

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ bundle install --path=vendor/bundle
 bundle exec cheatset generate commit_message_en.rb
 open commit_message_en.docset
 ```
+
+### Reference
+* [英語コミットコメントに使えるオシャレフレーズ集](http://qiita.com/ken_c_lo/items/4cb49f0fb74e8778804d)
--- commit_message_en.rb
+++ commit_message_en.rb
@@ -1,69 +1,142 @@
 
 cheatsheet do
-  title 'Git Commands'
-  docset_file_name 'git_commands'
-  keyword 'git'
+  title 'Commit Message In English'
+  docset_file_name 'commit_message_en'
+  keyword 'en'
   source_url 'http://cheat.kapeli.com'
 
   category do
-    id 'コミット'
+    id '単語集'
 
     entry do
-      name '部分的にコミット'
-      notes <<-'CODE'
-        ```bash
-        $ git add -p hoge.txt
-        ```
-      CODE
+      command 'undesirable'
+      name '好ましくない'
     end
-  end
-
-  category do
-    id '変更を退避'
-
     entry do
-      name '変更を一時退避'
-      notes <<-'CODE'
-        ```bash
-        $ git stash
-        ```
-      CODE
+      command 'avoid'
+      command 'circumvent'
+      name '回避する'
     end
-
     entry do
-      name '変更に名前をつけて退避'
-      notes <<-'CODE'
-        ```bash
-        $ git stash save 'hogehoge'
-        ```
-      CODE
+      command 'retrieve'
+      name '取りに行く(fetch / 「受け取る」よりもっと能動的に「取りに行く」ニュアンスを示す)'
     end
-
     entry do
-      name 'stashを一覧表示'
-      notes <<-'CODE'
-        ```bash
-        $ git stash list
-        ```
-      CODE
+      command 'revise'
+      name '= fix'
     end
-
     entry do
-      name '最新のstashを復元'
-      notes <<-'CODE'
-        ```bash
-        $ git stash pop
-        ```
-      CODE
+      command 'tweak'
+      name '微調整する'
     end
-
     entry do
-      name '特定のstashを復元'
-      notes <<-'CODE'
-        ```bash
-        $ git stash pop stash@{1}
-        ```
-      CODE
+      command 'in the sight of 〜'
+      name '〜の視点で (point of view をわざと仰々しく言った感じ)'
+    end
+    entry do
+      command 'refer'
+      name '参照する'
+    end
+    entry do
+      command 'disambiguate'
+      name '曖昧さをなくす'
+    end
+    entry do
+      command 'quite'
+      name 'とても（veryのかわりにつかえる'
+    end
+    entry do
+      command 'literally'
+      name '文字通り'
+    end
+    entry do
+      command 'exactly'
+      name 'まさに、確かに、イグザクトリー'
+    end
+    entry do
+      command 'resurrect'
+      name 'よみがえらせる'
+    end
+    entry do
+      command 'in favor of'
+      name '〜に賛成して, 何かに沿って'
+    end
+    entry do
+      command 'get rid of'
+      name '好ましくないものを取り除く'
+    end
+    entry do
+      command 'kick out'
+      name 'get rid of の強いバージョン、腹立たしさある'
+    end
+    entry do
+      command 'introduce'
+      name '導入する'
+    end
+    entry do
+      command 'extract'
+      name '抽出する'
+    end
+    entry do
+      command 'no longer used'
+      name 'もはや使われてない'
+    end
+    entry do
+      command 'R.I.P.'
+      name '安らかに眠れ（もう使ってないコードを消す時などに。「Removeでは語りきれない思いがあるんだ」 by_@idesaku_）'
+    end
+    entry do
+      command 'defeat'
+      name '倒す、打ち負かす'
+    end
+    entry do
+      command ':put_litter_in_its_place:'
+      name 'ゴミはゴミ箱へ（不要なコメントやwhitespaceを消したときなど）'
+    end
+    entry do
+      command 'cosmetic change, cosme'
+      name 'インデントを美しくした時など'
+    end
+    entry do
+      command 'fruitful'
+      name 'みのりの多い〜, fruitful discussion など'
+    end
+    entry do
+      command ':golf:'
+      name 'コードをより短くした（command golf）'
+    end
+    entry do
+      command 'Take a cup of coffee'
+      name 'JS を CoffeeScript 化した, またはCoffeeをよりCoffeeらしく書いた'
+    end
+    entry do
+      command 'kick in'
+      name '効きはじめる, 適用される など（DHHが使っていたらしい。）'
+    end
+    entry do
+      command 'A in B out'
+      name 'Bの変わりにAを導入（サッカー的な感じ。Gemの差し替えなどに使う）'
+    end
+    entry do
+      command 'enable to work'
+      name '動くようにした'
+    end
+    entry do
+      command 'wipe out'
+      name '拭い去る（get rid of, kick out の亜種）'
+    end
+    entry do
+      command 'stop rendering 〜'
+      name '〜を表示しない'
+    end
+    entry do
+      command 'aesthetic'
+      name 'cosmetic の亜種（多分）'
+    end
+    entry do
+      command 'Ooops!'
+      command ':scream:'
+      name 'しょうもないミスした時などに。:scream_cat:使うとかわいみでる。'
     end
   end
 end
